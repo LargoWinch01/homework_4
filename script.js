91883cc9801a9d2f7d03fa3e156a9ac352505959
LargoWinch01 <tokorobakidze7@gmail.com>
2024-09-04 16:46:33 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let age='17';
+
+if (age>=18 && typeof age === 'number'){
+    console.log('you can ');
+}
+else if (age<18 && typeof age === 'number') {
+    console.log('you can not');
+}
+else{
+    console.log('error');
+}
